b9e115fe39632ba8983e7d174ffdf5fbb1c6f7e8
steadykyu <awakekyu1@google.com>
2022-02-27 19:44:45 +0900

    그림추가

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -22,11 +22,11 @@ public class JpaMain {
             Long memberId = order.getMemberId();
             Member member = em.find(Member.class,memberId);
             // 식별자 값을 그대로 가져온 경우 - 더이상 참조를 할 수 없음
-            // 객체 지향적이지 않음
             // 객체 지향적이지 않은 관계형 DB에 맞춘 설계.
-            Member findmember = order.getMember();
+
+//            Member findmember = order.getMember();
             //Order 에 Member 필드를 추가하자
-            //객체를 가져와서 참조참조하는 객체지향적 모델
+            //객체를 가져와서 참조하는 객체지향적 모델
 
             tx.commit();
         } catch (Exception e){
--- jpashop/src/main/java/jpabook/jpashop/domian/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domian/Order.java
@@ -9,20 +9,12 @@ public class Order {
 
     @Id @GeneratedValue
     @Column(name = "ORDER_ID")
-    private Long id;
+    private Long id;    //pk
 
     @Column(name = "MEMBER_ID")
-    private Long memberId; //pk
+    private Long memberId; //fk
 
-    private Member member; // 객체지향적이기 위해 Member추가.
-
-    public Member getMember() {
-        return member;
-    }
-
-    public void setMember(Member member) {
-        this.member = member;
-    }
+//    private Member member; // 객체지향적이기 위해 Member추가.
 
     private LocalDateTime orderDate;    // DBA 분들이 원하는 테이블명 : ORDER_DATE, order_date
 
