608960f4aeb2fb11cc48f4bf681634f74f58a6d6
steadykyu <awakekyu1@google.com>
2022-03-02 21:45:29 +0900

    그림추가

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -2,6 +2,7 @@ package jpabook.jpashop;
 
 import jpabook.jpashop.domian.Member;
 import jpabook.jpashop.domian.Order;
+import jpabook.jpashop.domian.OrderItem;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -18,15 +19,8 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
-            Member member = em.find(Member.class,memberId);
-            // 식별자 값을 그대로 가져온 경우 - 더이상 참조를 할 수 없음
-            // 객체 지향적이지 않은 관계형 DB에 맞춘 설계.
-
-//            Member findmember = order.getMember();
-            //Order 에 Member 필드를 추가하자
-            //객체를 가져와서 참조하는 객체지향적 모델
+            Order order = new Order();
+            order.addOrderItem(new OrderItem());
 
             tx.commit();
         } catch (Exception e){
--- jpashop/src/main/java/jpabook/jpashop/domian/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domian/Order.java
@@ -2,6 +2,8 @@ package jpabook.jpashop.domian;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name ="ORDERS")
@@ -11,15 +13,25 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;    //pk
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId; //fk
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;            // 이전에 외래키로 직접가진 상태로 매핑하던 방식 fk
 
-//    private Member member; // 객체지향적이기 위해 Member추가.
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;              // 객체지향적이기 위해 Member추가.
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;    // DBA 분들이 원하는 테이블명 : ORDER_DATE, order_date
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 
     public Long getId() {
         return id;
@@ -29,13 +41,13 @@ public class Order {
         this.id = id;
     }
 
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
+//    public Long getMemberId() {
+//        return memberId;
+//    }
+//
+//    public void setMemberId(Long memberId) {
+//        this.memberId = memberId;
+//    }
 
     public LocalDateTime getOrderDate() {
         return orderDate;
@@ -52,4 +64,13 @@ public class Order {
     public void setStatus(OrderStatus status) {
         this.status = status;
     }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
 }
+
