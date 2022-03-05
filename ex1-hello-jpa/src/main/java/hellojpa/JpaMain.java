19481efe512e957e64fd5c143de696ea9c7f3bff
steadykyu <awakekyu1@google.com>
2022-03-06 02:52:13 +0900

    1차

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,11 @@
 package hellojpa;
 
+import hellojpa.advanced_mapping.Movie;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,17 +15,23 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
-            Member member = new Member();
-            member.setUsername("member1");
 
-            em.persist(member);
+            Movie movie = new Movie();
+            movie.setDirector("규하");
+            movie.setActor("박보영");
 
-            Team team = new Team();
-            team.setName("teamA");
-            //
-            team.getMembers().add(member);
+            //이름과 price는 Item에 getter setter가 있어야 메서드 호출이 가능능
+           movie.setName("바람과 함께 사라지다.");
+            movie.setPrice(10000);
 
-            em.persist(team);
+            em.persist(movie);
+            
+            em.flush();
+            em.clear();
+
+            Movie findMovie = em.find(Movie.class, movie.getId());
+            System.out.println("findMovie = " + findMovie);
+            // movie table이 item table과 inner join해서 값을 찾아내는 모습을 확인할수 있다.
 
             tx.commit();
 
