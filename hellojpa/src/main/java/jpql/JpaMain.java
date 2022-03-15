2bb7924cb54bdf2be4dfea7768a0850d37064fd1
steadykyu <awakekyu1@google.com>
2022-03-15 21:36:57 +0900

    완성

--- hellojpa/src/main/java/jpql/JpaMain.java
+++ hellojpa/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -11,30 +12,28 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try{
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB= new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
 
-            em.flush();
-            em.clear();
-
-
-            //사용자저으이
-            String query =
-                    "select group_concat(m.username) from Member m";       // Hibernate sql로 작성
-            //select function('group_concat', m.username) from Member m
-
-            List<String> result = em.createQuery(query, String.class)
-                    .getResultList();
-
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             tx.commit();
         } catch (Exception e){
