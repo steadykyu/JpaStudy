dadeb476b4f58170dea2bd92281990d84ebd51b8
steadykyu <awakekyu1@gmail.com>
2022-10-12 00:06:46 +0900

    복합키 공부

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,26 +20,16 @@ public class JpaMain {
         tx.begin();
         try{
 
-            //
-            Member member = new Member();
-            member.setUsername("member1");
-            em.persist(member);
+            Users user1 = new Users(1L,"userA",31);
+            Users user2 = new Users(2L,"userB",39);
+            Users user3 = new Users(3L,"userC",17);
+            em.persist(user1);
+            em.persist(user2);
+            em.persist(user3);
 
-            //flush -> commit, query생성시 내부에서 자동적으로 동작.
-            List<Member> resultList = em.createNativeQuery("select MEMBER_ID, city, street, zipcode, USERNAME FROM MEMBER", Member.class)
-                    .getResultList();
-
-            for(Member member1 : resultList){
-                System.out.println("member1 = " + member1); // member1의 값에 존재한다.
-            }
-            // 즉 flush()가 되고 쿼리를 생성한 것이다.
-
-            //예시코드로 아래처럼 쓰면 결과가 0이다.
-            // flush()가 되지않았기 떄문에 MEMBER TABLE에는 값이 없기 때문이다.
-            // 이럴때는 수동적으로 em.flush() 해주어 강제적으로 테이블에 값을 넣고 Member객체에 값을 채워넣어놔야한다.
-            // em.flush();
-//            dbconn.executeQuery("select * from member");
-
+            UsersId id2 = new UsersId(2L, "userB");
+            final Users findUser = em.find(Users.class, id2);
+            System.out.println("findUser = " + findUser);
 
             tx.commit();
         } catch (Exception e){
